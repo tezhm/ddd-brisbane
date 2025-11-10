7ea072c7c4f1418eeff038cbf26833d6088f943e
Terence Marks <tezmarks@gmail.com>
2025-11-10 17:44:37 +1000

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { PannaProvider } from "panna-sdk/react";
+import App from "./App.tsx"
+import "./index.css"
+import "panna-sdk/styles.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <PannaProvider clientId="b955e5ec244957bd248851c32e1647ad"
+                       partnerId="e99b37e4-3cff-4dfd-b5a2-73f68ed33978"
+                       chainId="4202">
+            <App />
+        </PannaProvider>
+    </StrictMode>,
+)
