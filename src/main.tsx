1ffbb070cde5373ed6b7575218950a323388f2b8
Terence Marks <tezmarks@gmail.com>
2025-11-10 20:02:17 +1000

    Adding tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { PannaProvider } from "panna-sdk/react";
+import "panna-sdk/styles.css";
 import App from "./App.tsx"
 import "./index.css"
-import "panna-sdk/styles.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
